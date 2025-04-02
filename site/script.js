8133dd2b2a1358afab0405a5f4bbd761b9a7a134
eyupemre05 <eeyup842@gmail.com>
2025-04-02 16:39:47 +0300

    okul

---
+++ site/script.js
@@ -0,0 +1,2 @@
+// Bu dosya, ilerleyen zamanlarda siteye interaktif özellikler eklemek için kullanılabilir.
+// Şu an boş bir dosya ama dinamik özellikler eklemek isterseniz buraya ekleme yapabilirsiniz.
